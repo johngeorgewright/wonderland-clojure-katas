415021b04a814df358b6953ee485cfaddb057115
John Wright <johngeorge.wright@gmail.com>
2014-12-28 12:25:16 +0000

    My first attempt at the alphabet cipher

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,43 @@
 (ns alphabet-cipher.coder)
 
+(use '[clojure.string :only (join)])
+
+(def alphabet ["a" "b" "c" "d" "e" "f" "g" "h" "i"
+               "j" "k" "l" "m" "n" "o" "p" "q" "r"
+               "s" "t" "u" "v" "w" "x" "y" "z"])
+
+(def alphabetCount (count alphabet))
+
+(defn encodeChar [keywordPos messagePos]
+  (let [encodedCharPos (+ messagePos keywordPos)
+        encodedCharPos (if (> encodedCharPos (dec alphabetCount))
+                         (- encodedCharPos alphabetCount)
+                         encodedCharPos)]
+    encodedCharPos))
+
+(defn decodeChar [keywordPos messagePos]
+  (let [decodedCharPos (- messagePos keywordPos)
+        decodedCharPos (if (< decodedCharPos 0)
+                         (+ decodedCharPos alphabetCount)
+                         decodedCharPos)]
+    decodedCharPos))
+
+(defn code [method keyword message]
+  (loop [keywordChars (seq keyword)
+         messageChars (seq message)
+         coded '()]
+    (if (empty? messageChars)
+      (join "" coded)
+      (let [keywordPos (.indexOf alphabet (str (first keywordChars)))
+            messagePos (.indexOf alphabet (str (first messageChars)))
+            moreKeywordChars (rest keywordChars)
+            codedCharPos (apply method [keywordPos messagePos])]
+        (recur (if (empty? moreKeywordChars) (seq keyword) moreKeywordChars)
+               (rest messageChars)
+               (concat coded (get alphabet codedCharPos)))))))
+
 (defn encode [keyword message]
-  "encodeme")
+  (code encodeChar keyword message))
 
 (defn decode [keyword message]
-  "decodeme")
+  (code decodeChar keyword message))
